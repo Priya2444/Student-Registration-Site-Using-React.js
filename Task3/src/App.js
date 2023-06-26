93cdc812e9be7ee8ccc97a581838d8abf840e0e7
Priya2444 <137447104+Priya2444@users.noreply.github.com>
2023-06-26 22:29:07 +0530

    Add files via upload

---
+++ Task3/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/navbar';
+import Form from './Components/form';
+function App() {
+  return (
+    <>
+    <Navbar></Navbar>
+    <Form></Form>
+    </>
+  );
+}
+
+export default App;
